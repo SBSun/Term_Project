64de73f37bf5442b29ebf3e6c5342d9691a2dabc
SBSun <46879319+SBSun@users.noreply.github.com>
2019-11-20 13:56:07 +0900

    1. SoundManager 작성 2. Sound 폴더 생성

---
+++ Term Project/Assets/Resource/Script/SoundManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SoundType
+{
+    BGM,
+    SFX
+}
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static SoundManager m_instance;
+    public static SoundManager instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                m_instance = Transform.FindObjectOfType<SoundManager>();
+            }
+            return m_instance;
+        }
+
+    }
+
+    public Dictionary<SoundType, List<AudioClip>> audioClips = new Dictionary<SoundType, List<AudioClip>>();
+
+    public AudioSource bgmPlayer;
+    public AudioSource sfxPlayer;
+
+    //------------------------------멤버 변수-------------------------------------
+
+    private void Awake()
+    {
+        if (m_instance == null)
+            m_instance = this;
+        else if (m_instance != this)
+            DestroyImmediate( this.gameObject );
+
+    }
+
+    void Start()
+    {
+        LoadFiles();
+    }
+
+    void LoadFiles()
+    {
+        AudioClip[] bgms = Resources.LoadAll<AudioClip>( "Sound/BGM" );
+        AudioClip[] SFXs = Resources.LoadAll<AudioClip>( "Sound/Effect" );
+
+        List<AudioClip> bgmList = new List<AudioClip>();
+        List<AudioClip> sfxList = new List<AudioClip>();
+
+        foreach (var bgm in bgms)
+        {
+            bgmList.Add( bgm );
+        }
+        audioClips.Add( SoundType.BGM, bgmList );
+
+        foreach (var sfx in SFXs)
+        {
+            sfxList.Add( sfx );
+        }
+        audioClips.Add( SoundType.SFX, sfxList );
+
+    }
+
+    public AudioClip FindAudioClip( string name, SoundType type )
+    {
+        AudioClip result = null;
+
+        List<AudioClip> list;
+        audioClips.TryGetValue( type, out list );
+
+        foreach (var sound in list)
+        {
+            if (sound.name == name)
+            {
+                result = sound;
+            }
+        }
+
+        return result;
+    }
+
+    public void PlayBGM()
+    {
+        bgmPlayer.Play();
+    }
+
+    public void PlayBGM( string name )
+    {
+        bgmPlayer.clip = FindAudioClip( name, SoundType.BGM );
+        bgmPlayer.Play();
+    }
+
+    public void StopBGM()
+    {
+        bgmPlayer.Stop();
+    }
+
+    public void PlaySFX( string name )
+    {
+        sfxPlayer.clip = FindAudioClip( name, SoundType.SFX );
+        sfxPlayer.Play();
+    }
+}
